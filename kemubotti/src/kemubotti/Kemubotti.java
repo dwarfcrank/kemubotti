2006906520391376120f251c5e64d3a6d7ed12b0
Matias Juntunen <matias.juntunen@gmail.com>
2012-10-30 17:45:28 +0200

    Initial commit

---
+++ kemubotti/src/kemubotti/Kemubotti.java
@@ -0,0 +1,15 @@
+package kemubotti;
+
+/**
+ *
+ * @author dwarfcrank
+ */
+public class Kemubotti {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
