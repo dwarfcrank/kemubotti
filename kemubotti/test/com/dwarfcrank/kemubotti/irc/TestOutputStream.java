3c06e57fdf4b054eb0e7af0af02074df0cd8c8d7
Matias Juntunen <matias.juntunen@gmail.com>
2012-11-20 21:25:17 +0200

    Updated TestOutputStream to handle strings better.

--- kemubotti/test/com/dwarfcrank/kemubotti/irc/TestOutputStream.java
+++ kemubotti/test/com/dwarfcrank/kemubotti/irc/TestOutputStream.java
@@ -6,6 +6,8 @@ package com.dwarfcrank.kemubotti.irc;
 
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -14,18 +16,31 @@ import java.io.OutputStream;
 public class TestOutputStream extends OutputStream {
 
     private StringBuilder stringBuilder;
+    private List<String> strings;
 
     public TestOutputStream() {
         stringBuilder = new StringBuilder();
+        strings = new ArrayList<String>();
     }
 
     @Override
     public void write(int i) throws IOException {
         stringBuilder.append((char) i);
+        
+        if((char)i == '\n') {
+            strings.add(stringBuilder.toString());
+            stringBuilder = new StringBuilder();
+        }
     }
 
     public String getString() {
-        return stringBuilder.toString();
+        if(strings.isEmpty()) {
+            return "";
+        }
+        
+        String ret = strings.get(0);
+        strings.remove(0);
+        return ret;
     }
 
     public void reset() {
