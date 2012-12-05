ebf721bdf342eea032d2246a4166a8077241b0bf
Matias Juntunen <matias.juntunen@gmail.com>
2012-12-05 08:41:33 +0200

    Added config system with test case.

---
+++ kemubotti/src/com/dwarfcrank/kemubotti/Config.java
@@ -0,0 +1,81 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dwarfcrank.kemubotti;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple class for storing configuration data. TODO: Serialization.
+ *
+ * @author dwarfcrank
+ */
+public class Config {
+
+    private static Map<String, String> strings = new HashMap<String, String>();
+    private static Map<String, Integer> integers = new HashMap<String, Integer>();
+
+    // Add some defaults
+    static {
+        setString("nick", "kemubotti");
+        setString("user_name", "kemubotti");
+        setString("real_name", "kemubotti");
+
+        setString("server_address", "underworld2.no.quakenet.org");
+        setInteger("server_port", 6667);
+
+        setString("channel", "#kemu-dev");
+    }
+
+    /**
+     * Gets a string value corresponding to the given key.
+     *
+     * @param key
+     * @return The string value corresponding the key, otherwise null.
+     */
+    public static String getString(String key) {
+        if (strings.containsKey(key)) {
+            return strings.get(key);
+        }
+
+        return null;
+    }
+
+    /**
+     * Gets an integer value corresponding to the given key.
+     *
+     * @param key
+     * @return The integer value corresponding the key, otherwise -1.
+     */
+    public static int getInteger(String key) {
+        if (integers.containsKey(key)) {
+            return integers.get(key);
+        }
+
+        return -1;
+    }
+
+    /**
+     * Sets the string value corresponding to the given key. Will overwrite the
+     * existing value, if any.
+     *
+     * @param key
+     * @param value
+     */
+    public static void setString(String key, String value) {
+        strings.put(key, value);
+    }
+
+    /**
+     * Sets the integer value corresponding to the given key. Will overwrite the
+     * existing value, if any.
+     *
+     * @param key
+     * @param value
+     */
+    public static void setInteger(String key, int value) {
+        integers.put(key, value);
+    }
+}
---
+++ kemubotti/test/com/dwarcrank/kemubotti/ConfigTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dwarcrank.kemubotti;
+
+import com.dwarfcrank.kemubotti.Config;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author dwarfcrank
+ */
+public class ConfigTest {
+
+    public ConfigTest() {
+    }
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() {
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    @Test
+    public void tryUnknownKeys() {
+        assertEquals(-1, Config.getInteger("asddgasgdjalxcvxcCV_XCVZBC"));
+        assertTrue(Config.getString("_:DGASDG;DG;Gxccgsagae") == null);
+    }
+
+    @Test
+    public void setAndGetString() {
+        Config.setString("a", "value");
+        assertEquals("value", Config.getString("a"));
+    }
+
+    @Test
+    public void setAndGetInteger() {
+        Config.setInteger("a", 1337);
+        assertEquals(1337, Config.getInteger("a"));
+    }
+
+    @Test
+    public void integerAndStringWithSameKey() {
+        String key = "this_is_a_key";
+
+        Config.setInteger(key, 1337);
+        Config.setString(key, "value");
+
+        assertEquals("value", Config.getString(key));
+        assertEquals(1337, Config.getInteger(key));
+    }
+}
