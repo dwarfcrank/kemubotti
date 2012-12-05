d0ce58200054350717745c11eac476b79bc84a7f
Matias Juntunen <matias.juntunen@gmail.com>
2012-12-05 11:24:16 +0200

    Added event parsing code.

--- kemubotti/src/com/dwarfcrank/kemubotti/Event.java
+++ kemubotti/src/com/dwarfcrank/kemubotti/Event.java
@@ -4,7 +4,12 @@
  */
 package com.dwarfcrank.kemubotti;
 
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -13,27 +18,60 @@ import java.util.Date;
 public class Event implements Comparable<Event> {
     private Date date;
     private String name;
-
+    
     public Event(Date date, String name) {
         this.date = date;
         this.name = name;
     }
-
+    
     public Date getDate() {
         return date;
     }
-
+    
     public String getName() {
         return name;
     }
-
+    
     @Override
     public int compareTo(Event t) {
         return date.compareTo(t.date);
     }
-
+    
     @Override
-    public String toString() {
+    public String toString() {        
         return date.toString() + " : " + name;
+    }
+    
+    private static Date parseDate(String text) {
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        
+        try {
+            Date d = df.parse(text);
+            return d;
+        } catch (ParseException ex) {
+            return null;
+        }
+    }
+    
+    public static Event parseEvent(String text) {
+        String[] parts = text.split(" ");
+        
+        Date d = null;
+        String eventName = "";
+        
+        for(int i = 0; i < parts.length; i++) {
+            if(d == null) {
+                d = parseDate(parts[i]);
+                continue;
+            }
+            
+            eventName += parts[i];
+            
+            if(i < (parts.length - 1)) {
+                eventName += " ";
+            }
+        }
+        
+        return new Event(d, eventName);
     }
 }
