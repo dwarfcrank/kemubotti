1825e2b98edae5e019e0643628c482982a85a427
Matias Juntunen <matias.juntunen@gmail.com>
2012-12-07 09:29:04 +0200

    Added Javadoc for CommandHandler.

--- kemubotti/src/com/dwarfcrank/kemubotti/CommandHandler.java
+++ kemubotti/src/com/dwarfcrank/kemubotti/CommandHandler.java
@@ -22,12 +22,30 @@ public abstract class CommandHandler {
         addHandler("listevents", new ListeventsHandler());
     }
     
+    /**
+     * Processes the command.
+     * @param sender The nickname of the user who issued this command.
+     * @param channel The channel this command was issued on.
+     * @param line The whole message that was sent to IRC.
+     */
     protected abstract void run(String sender, IRCChannel channel, String line);
     
+    /**
+     * Adds a handler for a command.
+     * @param command The command string to associate the handler with.
+     * @param handler The handler for the command.
+     */
     protected static void addHandler(String command, CommandHandler handler) {
         commands.put(command, handler);
     }
     
+    /**
+     * Locates the command handler for a given command and executes it.
+     * @param command The command to execute.
+     * @param sender The nickname of the user who issued the command.
+     * @param channel The channel this command was issued on.
+     * @param line The whole message that was sent to IRC.
+     */
     public static void HandleCommand(String command, String sender, IRCChannel channel, String line) {
         if(commands.containsKey(command)) {
             commands.get(command).run(sender, channel, line);
