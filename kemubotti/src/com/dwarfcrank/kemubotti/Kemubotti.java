310e402126d664ceca7fc7fe6868bac2a26ef3ce
Matias Juntunen <matias.juntunen@gmail.com>
2012-12-07 09:45:52 +0200

    Changed the Main method to simply catch any exception and quit.

--- kemubotti/src/com/dwarfcrank/kemubotti/Kemubotti.java
+++ kemubotti/src/com/dwarfcrank/kemubotti/Kemubotti.java
@@ -14,12 +14,16 @@ public class Kemubotti {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) throws UnknownHostException, IOException {
-        Socket s = new Socket(Config.getString("server_address"),
+    public static void main(String[] args) {
+        try {
+            Socket s = new Socket(Config.getString("server_address"),
                 Config.getInteger("server_port"));
-        
-        IRCServer server = IRCServer.connect(s);
-        
-        server.serverLoop();
+
+            IRCServer server = IRCServer.connect(s);
+
+            server.serverLoop();
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
