3715c43de47c2af0e561983c5f17f8336edcc6b0
Matias Juntunen <matias.juntunen@gmail.com>
2012-12-07 09:19:58 +0200

    Added Javadoc for AddeventHandler.

--- kemubotti/src/com/dwarfcrank/kemubotti/AddeventHandler.java
+++ kemubotti/src/com/dwarfcrank/kemubotti/AddeventHandler.java
@@ -12,6 +12,12 @@ import com.dwarfcrank.kemubotti.irc.IRCChannel;
  */
 public class AddeventHandler extends CommandHandler {
     
+    /**
+     * Adds an event suggestion to the event list.
+     * @param sender The nickname of the user who issued this command.
+     * @param channel The channel on which this command was issued.
+     * @param line The actual message that was sent to the IRC channel.
+     */
     @Override
     protected void run(String sender, IRCChannel channel, String line) {
         EventDatabase db = channel.getServer().getBot().getEventDatabase();
